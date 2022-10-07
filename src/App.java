dfc1a64b5d17ab633b71dd7e120c582e94e3d263
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-10-06 21:40:51 -0300

    First Vector

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] vetor = new int[5];
+        int[] dobro = new int[5];
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite 5 valores:");
+
+        for(int i = 0; i < 5; i++){
+            vetor[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < 5; i++){
+            dobro[i] = vetor[i]*2;
+        }
+
+        System.out.println("\nVetor\n");
+        
+        for(int i = 0; i < 5; i++){
+            System.out.print(vetor[i] +"\t");
+        }
+        
+        System.out.println("\nDobros\n");
+
+        for(int i = 0; i < 5; i++){
+            System.out.print(dobro[i] +"\t");
+        }
+        sc.close();
     }
 }
