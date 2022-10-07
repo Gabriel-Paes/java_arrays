11d4b009749a80f8b005fb97789408743f73ffe9
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-10-07 09:03:31 -0300

    Asking for vector size

--- src/App.java
+++ src/App.java
@@ -2,30 +2,33 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] vetor = new int[5];
-        int[] dobro = new int[5];
-
         Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Qual o tamanho do vetor?");
+        int n = sc.nextInt();
+        
+        int[] vetor = new int[n];
+        int[] dobro = new int[n];
 
-        System.out.println("Digite 5 valores:");
+        System.out.println("Digite "+ n +" valores:");
 
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < n; i++){
             vetor[i] = sc.nextInt();
         }
 
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < n; i++){
             dobro[i] = vetor[i]*2;
         }
 
         System.out.println("\nVetor\n");
         
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < n; i++){
             System.out.print(vetor[i] +"\t");
         }
         
         System.out.println("\nDobros\n");
 
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < n; i++){
             System.out.print(dobro[i] +"\t");
         }
         sc.close();
